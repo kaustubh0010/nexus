505cdeb5842da2dac762714c7ffcb7b955b1bff6
kaustubh0010 <kaustubhnadkar010@gmail.com>
2024-09-27 21:06:42 +0530

    Change icon colors

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -81,28 +81,28 @@ const MeetingTypeList = () => {
     return (
         <section className = "grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-4">
             <HomeCard
-                img="/icons/add-meeting.svg"
+                img="/icons/add-svgrepo-com.svg"
                 title="New Meeting"
                 description="Start an Instant meeting"
                 handleClick={() => setMeetingState('isInstantMeeting')}
                 className="bg-aqua-4"
             />
             <HomeCard
-                img="/icons/schedule.svg"
+                img="/icons/schedule-svgrepo-com.svg"
                 title="Schedule Meeting"
                 description="Plan your meeting"
                 handleClick={() => setMeetingState('isScheduleMeeting')}
                 className="bg-aqua-1"
             />
             <HomeCard
-                img="/icons/recordings.svg"
+                img="/icons/video-svgrepo-com.svg"
                 title="View Recordings"
                 description="Check out your recordings"
                 handleClick={() => router.push('/recordings')}
                 className="bg-aqua-3"
             />
             <HomeCard
-                img="/icons/join-meeting.svg"
+                img="/icons/add-profile-svgrepo-com.svg"
                 title="Join Meeting"
                 description="via Invitation Link"
                 handleClick={() => setMeetingState('isJoiningMeeting')}
